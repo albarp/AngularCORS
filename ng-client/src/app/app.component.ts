3abb4db86f1cac12137e68ead8427cfc7949b19d
Alberto A <alberto.arpini@fmc-ag.com>
2019-02-23 14:58:09 +0100

    added client local server call

--- ng-client/src/app/app.component.ts
+++ ng-client/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': 'authkey',
+      'userid': '1',
+      'Content-Type': 'text/plain'
+    })
+  };
+
   title = 'ng-client';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+
+    this.httpClient.get<any>('http://localhost:63888/', this.httpOptions).subscribe(
+      resp => {
+        this.title = resp.Success;
+      },
+      err => {
+        this.title = err;
+      }
+    );
+
+  }
 }
+
+
